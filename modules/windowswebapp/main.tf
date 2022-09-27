d26fbf542dffa22825f705462ac501605d5d39c8
Alberto Martín <martincasado.alberto@gmail.com>
2022-09-27 23:56:03 +0200

    🩹 Add missing tags

--- modules/windowswebapp/main.tf
+++ modules/windowswebapp/main.tf
@@ -26,6 +26,8 @@ resource "azurerm_log_analytics_workspace" "workspace" {
   resource_group_name = var.resource_group_name
   sku                 = "PerGB2018"
   retention_in_days   = 30
+
+  tags = var.tags
 }
 
 resource "azurerm_application_insights" "insights" {
